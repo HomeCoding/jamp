e466a6f3331b2641e75a754f2f296e5fefbbe851
taras <tgrynchuk@gmail.com>
2016-10-11 11:04:53 +0300

    enable async

--- src/main/java/com/jamp/app/controller/config/WebMvcContext.java
+++ src/main/java/com/jamp/app/controller/config/WebMvcContext.java
@@ -1,7 +1,9 @@
 package com.jamp.app.controller.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,6 +12,8 @@ import com.jamp.app.converter.LocalDateTimeConverter;
 
 @Configuration
 @EnableWebMvc
+//@EnableAsync
+//@EnableAspectJAutoProxy
 class WebMvcContext extends WebMvcConfigurerAdapter {
  
     @Override
