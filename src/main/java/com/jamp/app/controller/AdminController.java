6b3795ead653bdd5e005936ab01a938c67a947e6
Uhbyxer <tgrynchuk@gmail.com>
2017-07-16 17:27:24 +0300

    comment 1

--- src/main/java/com/jamp/app/controller/AdminController.java
+++ src/main/java/com/jamp/app/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//comment 1
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
